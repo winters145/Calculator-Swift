015117da30abf24945d2f9b8cf1717328128198e
Jack Winterschladen <winterschladen.j@gmail.com>
2022-08-09 12:50:22 +0100

    Update displayLabel with numbers pressed.

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -12,19 +12,27 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var displayLabel: UILabel!
     
-    
+    var isFinishedTypingNumber: Bool = true
     
     @IBAction func calcButtonPressed(_ sender: UIButton) {
         
         //What should happen when a non-number button is pressed
-    
+        isFinishedTypingNumber = true
     }
 
     
     @IBAction func numButtonPressed(_ sender: UIButton) {
         
         //What should happen when a number is entered into the keypad
-    
+        if let numValue = sender.currentTitle {
+            
+            if isFinishedTypingNumber {
+                displayLabel.text = numValue
+                isFinishedTypingNumber = false
+            } else {
+                displayLabel.text! += numValue
+            }
+        }
     }
 
 }
