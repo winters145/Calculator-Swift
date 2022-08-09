77af2e082e92c576c9d66e521038ed8f866a0a0b
Jack Winterschladen <winterschladen.j@gmail.com>
2022-08-09 14:12:15 +0100

    Refactor with computed displayValue variable.

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -14,19 +14,27 @@ class ViewController: UIViewController {
     
     private var isFinishedTypingNumber: Bool = true
     
+    private var displayValue: Double {
+        get {
+            guard let number = Double(displayLabel.text!) else {
+                fatalError("Cannot convert displayLabel text to a Double.")
+            }
+            return number
+        }
+        set {
+            displayLabel.text = String(newValue)
+        }
+    }
+    
     @IBAction func calcButtonPressed(_ sender: UIButton) {
         
         isFinishedTypingNumber = true
         
-        guard let number = Double(displayLabel.text!) else {
-            fatalError("Cannot convert displayLabel text to a Double.")
-        }
-        
         if let calcMethod = sender.currentTitle {
             if calcMethod == "+/-" {
-                displayLabel.text = String(number * -1)
+                displayValue *= -1
             } else if calcMethod == "%" {
-                displayLabel.text = String(number / 100)
+                displayValue /= 100
             } else if calcMethod == "AC" {
                 displayLabel.text = "0"
             }
@@ -39,9 +47,26 @@ class ViewController: UIViewController {
         if let numValue = sender.currentTitle {
             
             if isFinishedTypingNumber {
-                displayLabel.text = numValue
+                if numValue == "." {
+                    displayLabel.text = "0."
+                } else {
+                    displayLabel.text = numValue
+                }
                 isFinishedTypingNumber = false
             } else {
+                
+                if numValue == "." {
+
+                    guard let currentDisplayValue = Double(displayLabel.text!) else {
+                        fatalError("Cannot convert display label text to a Double.")
+                    }
+                    
+                    let isInt = floor(currentDisplayValue) == Double(currentDisplayValue)
+                    
+                    if !isInt {
+                        return
+                    }
+                }
                 displayLabel.text! += numValue
             }
         }
